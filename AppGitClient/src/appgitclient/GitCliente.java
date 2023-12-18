cc17f582806726b760a6bf51a347cd1f9263e73b
aresmargal <151525545+aresmargal@users.noreply.github.com>
2023-12-18 11:51:12 +0100

    Add files via upload

---
+++ AppGitClient/src/appgitclient/GitCliente.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appgitclient;
+
+/**
+ *
+ * @author margalar
+ */
+public class GitCliente {
+    
+    private String cliente;
+    private String version; 
+    private String sistemOper; 
+    private String licencia; 
+    
+    public GitCliente(){
+        
+    }
+    
+    public String getCliente(){
+        return cliente; 
+    }
+    
+    public void setCliente(String cliente){
+        this.cliente = cliente;
+    }
+    
+    public String getVersion(){
+        return version; 
+    }
+    
+    public void setVersion(String version){
+        this.version = version; 
+    }
+    
+    public void setSistemOper(String sistemOper){
+        this.sistemOper = sistemOper;
+    }
+    
+    public String getLicencia(){
+        return licencia; 
+    }
+    
+    public void setLicencia(String licencia){
+    this.licencia = licencia; 
+    }
+    @Override 
+    public String toString(){
+        return "GtCliente{" + "cliente=" + cliente + ", version= " + version + ", sistemOper =" + sistemOper + ", licencia=" + licencia + "}";
+    }
+}
